016d8614c3900c5b0ec47a9c33be69a9f5f044f3
Ceacontabil <113186618+Ceacontabil@users.noreply.github.com>
2022-09-11 15:41:03 -0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import App from './app';
 import "bootstrap-icons/font/bootstrap-icons.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App></App>
+    <HashRouter>
+      <App></App>
+    </HashRouter>
   </React.StrictMode>
 );
