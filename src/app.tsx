69c38c5e073c38c681c5c1da1f6f5aea88605e13
Ceacontabil <113186618+Ceacontabil@users.noreply.github.com>
2022-09-11 18:57:20 -0300

    Update app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,15 @@
-import { Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import {  } from 'react-router-dom';
 import Home from './pages/Home';
 import Send from './pages/Send';
 
 export default function App() {
     return(
+        <HashRouter>
             <Routes>
                 <Route path='/' element={<Home/>}/>
                 <Route path='/enviado' element={<Send/>}/>
             </Routes>
+        </HashRouter>
     )
 }
