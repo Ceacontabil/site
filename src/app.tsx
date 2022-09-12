4e6d3a24dbc575b5ec6cf2efb6e7d9c1d333a8cc
Ceacontabil <113186618+Ceacontabil@users.noreply.github.com>
2022-09-11 23:26:14 -0300

    Update app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,14 @@
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Send from './pages/Send';
 
 export default function App() {
     return(
-        <HashRouter basename={process.env.PUBLIC_URL+'/'}>
+        <BrowserRouter basename={process.env.PUBLIC_URL+'/'}>
             <Routes>
                 <Route path='/' element={ <Home/> } />
                 <Route path='/enviado' element={ <Send/> } />
             </Routes>
-        </HashRouter>
+        </BrowserRouter>
     )
 }
